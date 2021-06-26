c59a38e868e81162ec1b93e75c1ba205d5c66cc9
IsabellaSchneider7 <71522975+IsabellaSchneider7@users.noreply.github.com>
2021-06-26 01:34:49 -0400

    firebase working

---
+++ studyBuddiesProject/src/Queue.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import VideoChat from './VideoChat';
+
+function Queue(){
+    return(
+    <div>
+      <main>
+          <VideoChat />
+        </main>
+    </div>)
+
+  }
+
+  export default Queue;
