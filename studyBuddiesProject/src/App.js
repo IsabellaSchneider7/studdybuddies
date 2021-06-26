8f81294484705be897e9358aabf42992640b6ef9
Ria Narang <rianarang@Rias-MacBook-Pro.local>
2021-06-25 22:01:54 -0400

    LETS GOOO SHE WORKS

--- studyBuddiesProject/src/App.js
+++ studyBuddiesProject/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="app">
       <header>
-        <h1>Video Chat with Hooks</h1>
+        <h1>StudyBuddies</h1>
       </header>
       <main>
         <VideoChat />
